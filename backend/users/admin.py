7eaa7688c0c927465481cd495ce3a9a04dce8a53
Yan-Zhelanov <yzhelanov@gmail.com>
2021-09-24 20:59:45 +0300

    v0.1 — Fixed: set password for users by admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,36 @@
-from django.contrib import admin
+from django.contrib.admin import register
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import User
+
+
+@register(User)
+class UserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = (
+        'id', 'email', 'username', 'first_name', 'last_name', 'is_blocked',
+        'is_superuser',
+    )
+    list_filter = (
+        'is_blocked', 'is_superuser',
+    )
+    fieldsets = (
+        (None, {'fields': (
+            'email', 'username', 'first_name', 'last_name', 'password',
+        )}),
+        ('Permissions', {'fields': ('is_blocked', 'is_superuser',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'username', 'first_name', 'last_name', 'password1', 'password2', 'is_blocked',
+                'is_superuser',
+            )
+        }),
+    )
+    search_fields = ('email', 'username', 'first_name', 'last_name',)
+    ordering = ('id', 'email', 'username',)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,16 +1,17 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db.models import EmailField, CharField, BooleanField
 
 from .managers import UserManager
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     email = EmailField(max_length=254, unique=True)
     username = CharField(max_length=150)
     first_name = CharField(max_length=150)
     last_name = CharField(max_length=150)
     password = CharField(max_length=150)
     is_superuser = BooleanField(default=False)
+    is_blocked = BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -19,4 +20,8 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.username
+
+    @property
+    def is_staff(self):
+        return self.is_superuser
     
